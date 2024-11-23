428ffdf2086298c56d8708806aff50167615e471
sajal-29 <sajaljain2003@gmail.com>
2024-11-23 05:43:33 +0530

    files modified

--- frontend/pdf_man/lib/main.dart
+++ frontend/pdf_man/lib/main.dart
@@ -9,12 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.grey[900],
-        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
-      ),
+      debugShowCheckedModeBanner: false,      
       home: DocxToPdfConverter(),
     );
   }
